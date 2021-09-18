0ed110015d311bde72989b8817fdd2c96f3f82d7
Eeshita <73847510+eeshitaa@users.noreply.github.com>
2021-09-18 23:17:15 +0530

    Changed JS and HTML to make API work

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -1,18 +1,18 @@
-
 $(document).ready(function(){
+$("h1").text("Hello");
 var api_key="at_js9ePvEApOubx3TuhLWH67GGb41sq";
 var ip;
-// position icon 
-var iconmarker=L.icon({
-    iconUrl:'../images/icon-location.svg';
+// position icon
+var iconmarker = new L.icon({
+    iconUrl:'images/icon-location.svg',
     iconSize:[38,95],
-    shadowSize:[ 50,64];
-    iconAnchor:[22,94];
-    shadowAnchor:[4,62];
+    shadowSize:[50,64],
+    iconAnchor:[22,94],
+    shadowAnchor:[4,62]
     //popupAnchor[-3,-76];
 });
 
-var mappos=L.map('mapid').setView([28.505, 77.09], 13);
+var mappos = L.map('mapid').setView([28.505, 77.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibm9oYW1vaGFtbWVkIiwiYSI6ImNrZ2xiZG5idTByamkzMG5hb3JrdG5mazQifQ.kn4WxorvMQJI5Q8GA4O13A', {
         maxZoom: 18,
@@ -25,26 +25,24 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     }).addTo(mappos);
 
     var marker = L.marker([28.505, 77.09], {icon: iconmarker}).addTo(mappos);
-    
-
 
 
 $('form').submit(function(e){
 //CHECK
     e.preventDefault();
-    ip=$('.search').val();
-$.get("https://geo.ipify.org/api/v1?apiKey")+api_key+"&ipAddress="+ip,function(container
+    ip=$('.search-box').val();
+$.get("https://geo.ipify.org/api/v1?apiKey="+api_key+"&ipAddress="+ip,function(data)
 {
-    $('.card-ip-text').html(container.ip);
+    $('.card-ip-text').html(data.ip);
 
-    $('.card-loc-text').html(container.location.city + ', ' + container.location.region + ' ' + container.location.postalCode);
+    $('.card-loc-text').html(data.location.city + ', ' + data.location.region + ' ' + data.location.postalCode);
+ss
+    $('.card-ist-text').html('IST' + data.location.timezone);
 
-    $('.card-ist-text').html('IST' + container.location.timezone);
+    $('.card-isp-text').html(data.isp);
+    mappos.setView([data.location.lat, data.location.lng], 15);
 
-    $('.card-isp-text').html(container.isp);
-    mappos.setView([card.location.lat, card.location.lng], 15);
-
-    marker = new L.marker([card.location.lat, card.location.lng], {icon: iconmarker}).addTo(mappos);
+    marker = new L.marker([data.location.lat, data.location.lng], {icon: iconmarker}).addTo(mappos);
 });
 });
 });
