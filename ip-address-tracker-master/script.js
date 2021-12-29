9237561d8a4ac985e5a9b9f55a97e3ec93385e78
Eeshita <73847510+eeshitaa@users.noreply.github.com>
2021-12-29 23:37:57 +0530

    Added weather functionality

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -4,7 +4,7 @@ var ip;
 // position icon
 var iconmarker = new L.icon({
     iconUrl:'images/icon-location.svg',
-    iconSize:[38,95],
+    iconSize:[40,60],
     shadowSize:[50,64],
     iconAnchor:[22,94],
     shadowAnchor:[4,62]
@@ -23,43 +23,99 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
         zoomOffset: -1
     }).addTo(mappos);
 
-    var marker = L.marker([28.505, 77.09], {icon: iconmarker}).addTo(mappos);
+var marker = L.marker([28.505, 77.09], {icon: iconmarker}).addTo(mappos);
 
+var wind=document.querySelector('.wind');
+var desc =document.querySelector('.desc');
+var temp=document.querySelector('.temp');
+var groups;
+console.log("1!!!");
 
-$('form').submit(function(e){
-//CHECK
+$('form').submit(function(e) {
     e.preventDefault();
     ip=$('.search-box').val();
-$.get("https://geo.ipify.org/api/v1?apiKey="+api_key+"&ipAddress="+ip,function(data)
-{
-    $('.card-ip-text').html(data.ip);
+    $.get("https://geo.ipify.org/api/v1?apiKey="+api_key+"&ipAddress="+ip,function(data)
+    {
+        $('.card-ip-text').html(data.ip);
 
-    $('.card-loc-text').html(data.location.city + ', ' + data.location.region + ' ' + data.location.postalCode);
+        $('.card-loc-text').html(data.location.city + ', ' + data.location.region + ' ' + data.location.postalCode);
 
-    $('.card-ist-text').html('IST' + data.location.timezone);
+        $('.card-ist-text').html('IST' + data.location.timezone);
 
-    $('.card-isp-text').html(data.isp);
-    mappos.setView([data.location.lat, data.location.lng], 15);
+        $('.card-isp-text').html(data.isp);
+        mappos.setView([data.location.lat, data.location.lng], 15);
+        marker = new L.marker([data.location.lat, data.location.lng], {icon: iconmarker}).addTo(mappos);
 
-    marker = new L.marker([data.location.lat, data.location.lng], {icon: iconmarker}).addTo(mappos);
+        fetch('http://api.openweathermap.org/data/2.5/weather?units=metric&q='+ data.location.city +'&APPID=a863866c91b3f842a1b13b78759f5e0e')
+        .then(response => response.json())
+        .then(dt=> {
+            console.log(dt);
+            var windvalue=dt['wind']['speed'];
+            var tempval=dt['main']['temp'];
+            var descvalue=dt['weather'][0]['description'];
+            groups = dt['weather'][0]['main'];
+            // groups = prompt('Enter weather: ');
+            console.log(groups)
+            console.log("2!!!");
+            wind.innerHTML = windvalue + "M/S";
+            temp.innerHTML = tempval + "&#176;C";
+            desc.innerHTML = descvalue.toUpperCase();
+            console.log("3!!!");
+            icons = document.querySelector('.iconweather');
+
+            if (groups == 'Thunderstorm') {
+              icons.innerHTML = "<img src='animated/thunder-cropped.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/thunder.jpg')";
+              document.querySelector('.weathertable').style.color = 'white';
+              document.querySelector('h1').style.color = 'white';
+            }
+
+            else if (groups == 'Drizzle') {
+              icons.innerHTML = "<img src='animated/rainy-4-cropped.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/rain.jpg')";
+              document.querySelector('.weathertable').style.color = 'white';
+              document.querySelector('h1').style.color = 'white';
+            }
+
+            else if (groups == 'Rain') {
+              icons.innerHTML = "<img src='animated/rainy-6-cropped.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/rain.jpg')";
+              document.querySelector('.weathertable').style.color = 'white';
+              document.querySelector('h1').style.color = 'white';
+            }
+
+            else if(groups == 'Snow') {
+              icons.innerHTML = "<img src='animated/snowy-5-cropped.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/snow.jpg')";
+              document.querySelector('.weathertable').style.color = 'white';
+              document.querySelector('h1').style.color = 'white';
+            }
+
+            else if(groups == 'Clear') {
+              icons.innerHTML = "<img src='animated/day-cropped.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/sun.jpg')";
+              document.querySelector('.weathertable').style.color = 'black';
+              document.querySelector('h1').style.color = 'black';
+            }
+
+            else if(groups == 'Clouds') {
+              icons.innerHTML = "<img src='animated/cloudy-cropped.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/cloud.jpg')";
+              document.querySelector('.weathertable').style.color = 'darkblue';
+              document.querySelector('h1').style.color = 'darkblue';
+            }
+
+            else if(groups == 'Atmosphere') {
+              icons.innerHTML = "<img src='animated/weather_sunset.svg' class='icond'/>";
+              document.querySelector('#titles').style.backgroundImage = "url('images/fog.jpg')";
+              document.querySelector('.weathertable').style.color = 'white';
+              document.querySelector('h1').style.color = 'white';
+            }
+        })
+        .catch(err=>alert("Wrong Place"))
+    });
 });
-});
-});
 
-var names=document.querySelector('.names');
-var desc =document.querySelector('.desc');
-var temp=document.querySelector('.tempval')
 
-$('form').submit(function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+data.location.city.value+'&APPID=a863866c91b3f842a1b13b78759f5e0e').then(
-        response => response.json()).then(data=>{var namevalue=data['name'];
-        var tempval=data['main']['temp'];
-        var descvalue=data['weather'][0]['description'];
-    
-        names.innerHTML=namevalue;
-        temp.innerHTML=tempval;
-        desc.innerHTML=descvalue;
-    
-    })
-    .catch(err=>alert("wrong place"))
-})
+
+});
